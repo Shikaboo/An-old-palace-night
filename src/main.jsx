a92494fb09851f8b18f596fe018491231f807c74
Shikaboo <sksdhkd0724@gmail.com>
2024-08-07 02:56:10 +0900

    first 팀 프로젝트 다른 원격 저장소로 옮기기

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./common.css"
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename={'/Team_Period/'}>
+  <BrowserRouter basename={'/an-old-palace-night/'}>
   <App />
   </BrowserRouter>
 )
